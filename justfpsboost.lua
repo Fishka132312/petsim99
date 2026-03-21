ff5f390ed7217298117c27f84d37bf75bc2988ee
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-21 08:20:42 +0100

    Update justfpsboost.lua

--- justfpsboost.lua
+++ justfpsboost.lua
@@ -5,4 +5,3 @@ wait(1)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/antilag.lua'))()
 wait(1)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/deleteguis.lua'))()
-end
