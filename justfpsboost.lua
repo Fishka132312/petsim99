04ba68ad227900e3224e9f9451ae437fd2221bd4
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-21 09:33:36 +0100

    Update justfpsboost.lua

--- justfpsboost.lua
+++ justfpsboost.lua
@@ -1,24 +1,10 @@
-local function safeLoad(url)
-    local success, res = pcall(function()
-        return game:HttpGet(url)
+local base = "https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/"
+
+for _,v in ipairs({"fpsboost2.lua","petsoptimizer.lua","antilag.lua","deleteguis.lua"}) do
+    task.spawn(function()
+        pcall(function()
+            loadstring(game:HttpGet(base..v))()
+        end)
     end)
-    if success and res then
-        local func, err = loadstring(res)
-        if func then
-            pcall(func)
-            print("Loaded successfully")
-        else
-            warn("Syntax error in script: " .. err)
-        end
-    else
-        warn("Failed to fetch script from URL")
-    end
+    task.wait(1)
 end
-
-safeLoad('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/deleteguis.lua')
-task.wait(1.5)
-
-safeLoad('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/fpsboost2.lua')
-task.wait(1.5)
-
-safeLoad('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/antilag.lua')
