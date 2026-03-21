da41a768e4265fe3c5ab5689ca65a348f0ff8a5d
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-21 09:34:08 +0100

    Update autofarmraid+zaphub.lua

--- autofarmraid+zaphub.lua
+++ autofarmraid+zaphub.lua
@@ -1,23 +1,23 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/deleteguis.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/fpsboost2.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/autotap.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/automagnet.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/autogifts.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/auto%20titanic.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/antilag.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Raidfarm.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/turnoff3d.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Raidfarm.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/serverhoptime.lua'))()
-wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/zaphub.lua'))()
+local base = "https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/"
+
+local scripts = {
+    "deleteguis.lua",
+    "fpsboost2.lua",
+    "autotap.lua",
+    "automagnet.lua",
+    "autogifts.lua",
+    "auto%20titanic.lua",
+    "antilag.lua",
+    "Raidfarm.lua",
+    "turnoff3d.lua",
+    "Raidfarm.lua",
+    "serverhoptime.lua",
+    "zaphub.lua"
+}
+
+for _,v in ipairs(scripts) do
+    pcall(function()
+        loadstring(game:HttpGet(base..v))()
+    end)
+    task.wait(1)
+end
