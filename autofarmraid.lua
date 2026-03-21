37386d2231dac4fb19c4005e19af4c7fe337b157
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-21 11:06:57 +0100

    Update autofarmraid.lua

--- autofarmraid.lua
+++ autofarmraid.lua
@@ -1,7 +1,7 @@
 local scripts = {
     "deleteguis.lua", "fpsboost2.lua", "autotap.lua", 
     "automagnet.lua", "autogifts.lua", "auto%20titanic.lua", 
-    "antilag.lua", "Raidfarm.lua", "turnoff3d.lua", "serverhoptime.lua"
+    "antilag.lua", "Raidfarm.lua", "turnoff3d.lua", "serverhoptime.lua", "megaspeedpets.lua"
 }
 
 local baseUrl = "https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/"
