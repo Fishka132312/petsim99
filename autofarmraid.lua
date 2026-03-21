d500f94c558b11d47bbc8e48860d91c502a6e391
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-21 07:15:08 +0100

    Update autofarmraid.lua

--- autofarmraid.lua
+++ autofarmraid.lua
@@ -17,4 +17,6 @@ wait(1)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99farmantilag/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/turnoff3d.lua'))()
 wait(1)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99farmantilag/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Raidfarm.lua'))()
+wait(1)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99farmantilag/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/serverhoptime.lua'))()
 end
