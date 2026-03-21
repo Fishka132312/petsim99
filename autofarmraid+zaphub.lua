eed3ee4fd6c170c4393e35fdc1422c303cb31a74
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-21 07:15:19 +0100

    Update autofarmraid+zaphub.lua

--- autofarmraid+zaphub.lua
+++ autofarmraid+zaphub.lua
@@ -18,5 +18,7 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99
 wait(1)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99farmantilag/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Raidfarm.lua'))()
 wait(1)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99farmantilag/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/serverhoptime.lua'))()
+wait(1)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99farmantilag/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/zaphub.lua'))()
 end
