7516ed1bd50e20fa2c4b12ecc5185eb481f84171
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-22 08:47:06 +0100

    Update turnoff3d.lua

--- все нужное/turnoff3d.lua
+++ все нужное/turnoff3d.lua
@@ -1,1 +1,7 @@
-game:GetService("RunService"):Set3dRenderingEnabled(false)
+local visual = _G.VisualConfig or {}
+
+if visual.Disable3D == true then
+    game:GetService("RunService"):Set3dRenderingEnabled(false)
+elseif visual.Disable3D == false then
+    game:GetService("RunService"):Set3dRenderingEnabled(true)
+end
