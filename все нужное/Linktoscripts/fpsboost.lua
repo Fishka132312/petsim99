e13acccb3f95c5abe5994cca31490e3ff1799bc2
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-22 09:32:53 +0100

    Update fpsboost.lua

--- все нужное/Linktoscripts/fpsboost.lua
+++ все нужное/Linktoscripts/fpsboost.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/justfpsboost.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/shortlink/JustFpsBoost.lua'))()
