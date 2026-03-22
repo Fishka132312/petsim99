9e7087afd5a74cd479db1e84dc5a03653d45e233
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-22 09:31:09 +0100

    Add files via upload

---
+++ все нужное/Antilag/Disable3d.lua
@@ -0,0 +1,7 @@
+local visual = _G.VisualConfig or {}
+
+if visual.Disable3D == true then
+    game:GetService("RunService"):Set3dRenderingEnabled(false)
+elseif visual.Disable3D == false then
+    game:GetService("RunService"):Set3dRenderingEnabled(true)
+end
