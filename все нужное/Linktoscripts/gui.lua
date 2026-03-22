23ea8a3618871265cd724e9ce6f07ede1cecc1a8
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-22 09:06:49 +0100

    Add files via upload

---
+++ все нужное/Linktoscripts/gui.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Gui/petsim.lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Gui/petsimmygui.lua'))()
