9a6d034ac95e6b2ddefaabf520ccbc8f72a2ee7a
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-22 09:11:58 +0100

    Update autoafarmraid.lua

--- все нужное/Linktoscripts/autoafarmraid.lua
+++ все нужное/Linktoscripts/autoafarmraid.lua
@@ -18,4 +18,4 @@ _G.VisualConfig = {
     Disable3D = true,
 }
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/autofarmraid.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/AutoFarmFull/AutoFarmRaid.lua'))()
