44e9f6688900c6c671cc40627c07384ba089126d
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-23 21:53:47 +0100

    Update antiafk.lua

--- все нужное/antiafk.lua
+++ все нужное/antiafk.lua
@@ -1,14 +1,6 @@
-local VirtualUser = game:GetService("VirtualUser")
-local VirtualInputManager = game:GetService("VirtualInputManager")
-
-game.Players.LocalPlayer.Idled:Connect(function()
-    VirtualUser:CaptureController()
-    VirtualUser:ClickButton2(Vector2.new())
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
-
-while true do
-    task.wait(60)
-    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.W, false, game)
-    task.wait(1)
-    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.W, false, game)
-end
