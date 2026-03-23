82a32e133b3874eddd787e1a23fc96f2a4d650a9
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-23 17:31:59 +0100

    Update antiafk.lua

--- все нужное/antiafk.lua
+++ все нужное/antiafk.lua
@@ -1,1 +1,20 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hassanxzayn-lua/Anti-afk/main/antiafkbyhassanxzyn"))();
+local Players = game:GetService("Players")
+local VirtualInputManager = game:GetService("VirtualInputManager")
+local player = Players.LocalPlayer
+
+while true do
+    task.wait(300)
+    
+    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+        
+        VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.W, false, game)
+        task.wait(0.5)
+        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.W, false, game)
+        
+        task.wait(0.2)
+        
+        VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.S, false, game)
+        task.wait(0.5)
+        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.S, false, game)
+    end
+end
