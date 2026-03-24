f9f5c13bfb626a691041a9479df052174bffb0ac
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-25 00:22:40 +0100

    Update FpsCap.lua

--- все нужное/Antilag/FpsCap.lua
+++ все нужное/Antilag/FpsCap.lua
@@ -1,12 +1,10 @@
-_G.FPS_Enabled = _G.FPS_Enabled or false
-_G.FPS_Value = _G.FPS_Value or 60
-
-print("Движок FPS запущен и ждет настроек из конфига...")
+_G.FPS_Enabled = false
+_G.FPS_Value = 60
 
 task.spawn(function()
     while true do
-        if _G.FPS_Enabled == true then
-            setfpscap(_G.FPS_Value)
+        if _G.FPS_Enabled then
+            setfpscap(tonumber(_G.FPS_Value) or 60)
         else
             setfpscap(999)
         end
