9875aac74033cb2762cacd862e15359cb5d68529
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-24 20:21:50 +0100

    Update AutoBuyNewZone.lua

--- все нужное/things/AutoBuyNewZone.lua
+++ все нужное/things/AutoBuyNewZone.lua
@@ -1,23 +1,23 @@
-_G.AutoBuyEnabled = false 
+_G.AutoBuyEnabled = false
 
 task.spawn(function()
     local ZoneCmds = require(game.ReplicatedStorage.Library.Client.ZoneCmds)
     local ZonesUtil = require(game.ReplicatedStorage.Library.Util.ZonesUtil)
     local Network = require(game.ReplicatedStorage.Library.Client.Network)
 
-    print("Авто-покупка запущена. Статус: " .. tostring(_G.AutoBuyEnabled))
+    print("Скрипт загружен. Ожидание включения (AutoBuyEnabled = true)...")
 
     while true do
-        if _G.AutoBuyEnabled then
+        if _G.AutoBuyEnabled == true then
             local _, maxZoneData = ZoneCmds.GetMaxOwnedZone()
             
             if maxZoneData and maxZoneData.ZoneNumber then
                 local nextZoneName, nextZoneData = ZonesUtil.GetZoneFromNumber(maxZoneData.ZoneNumber + 1)
                 
                 if nextZoneData and nextZoneData.ZoneName then
-                    local success, result = Network.Invoke("Zones_RequestPurchase", nextZoneData.ZoneName)
+                    local success = Network.Invoke("Zones_RequestPurchase", nextZoneData.ZoneName)
                     if success then
-                        print("Куплена зона: " .. nextZoneData.ZoneName)
+                        print("Успешно куплена зона: " .. nextZoneData.ZoneName)
                     end
                 end
             end
