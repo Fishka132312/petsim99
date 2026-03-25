c3f692b6dd71c284bf4c0e9c3da18e664e1ce190
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-25 02:13:43 +0100

    Update Autodoranks.lua

--- все нужное/AutoRank/Autodoranks.lua
+++ все нужное/AutoRank/Autodoranks.lua
@@ -3,7 +3,7 @@ _G.Autorank = false
 local Save = require(game.ReplicatedStorage.Library.Client.Save)
 local QuestCmds = require(game.ReplicatedStorage.Library.Client.QuestCmds)
 
-local function updateStates(needsFarm, needsJar, needsComet, needsHatch)
+local function updateStates(needsFarm, needsJar, needsComet, needsHatch, potionTier)
     -- Фарм и передвижение
     _G.AutoSpeedPetsForRank = needsFarm
     _G.AutoMagnetForRank = needsFarm
@@ -16,16 +16,22 @@ local function updateStates(needsFarm, needsJar, needsComet, needsHatch)
     
     -- Открытие яиц
     _G.AutoHatchBestEggForRank = needsHatch
+    
+    -- Квест на зелья
+    -- Если potionTier больше 0, значит квест активен
+    _G.AutoUsePotionsForRank = (potionTier > 0)
+    _G.PotionTierToUse = potionTier 
 end
 
 task.spawn(function()
-    print("Авто-ранг менеджер (v4) запущен!")
+    print("Авто-ранг менеджер (v5) запущен!")
     
     while true do
         local needsFarm = false
         local needsJar = false
         local needsComet = false
         local needsHatch = false
+        local potionTier = 0 -- 0 означает, что квеста нет
 
         if _G.Autorank then
             local data = Save.Get()
@@ -34,8 +40,7 @@ task.spawn(function()
                 for _, goal in pairs(data.Goals) do
                     local title = string.lower(QuestCmds.MakeTitle(goal))
                     
-                    -- 1. Квесты в лучшей зоне (Best Area или Алмазы)
-                    -- Алмазы почти всегда эффективнее бить в лучшей зоне
+                    -- 1. Квесты в лучшей зоне
                     if string.find(title, "best area") or string.find(title, "diamonds") then
                         needsFarm = true
                     end
@@ -56,12 +61,21 @@ task.spawn(function()
                     if string.find(title, "hatch") and string.find(title, "best egg") then
                         needsHatch = true
                     end
+
+                    -- 5. Квесты на Зелья (Tier X)
+                    if string.find(title, "tier") and string.find(title, "potion") then
+                        -- Ищем цифру после слова "tier"
+                        local tier = string.match(title, "tier%s+(%d+)")
+                        if tier then
+                            potionTier = tonumber(tier)
+                        end
+                    end
                 end
             end
         end
 
         -- Обновляем все переменные
-        updateStates(needsFarm, needsJar, needsComet, needsHatch)
+        updateStates(needsFarm, needsJar, needsComet, needsHatch, potionTier)
         
         task.wait(2)
     end
