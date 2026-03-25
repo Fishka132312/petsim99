a7e9a4de2b1e9faa3c6535476529735d802061bd
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-25 01:47:40 +0100

    Update Autodoranks.lua

--- все нужное/AutoRank/Autodoranks.lua
+++ все нужное/AutoRank/Autodoranks.lua
@@ -3,23 +3,29 @@ _G.Autorank = false
 local Save = require(game.ReplicatedStorage.Library.Client.Save)
 local QuestCmds = require(game.ReplicatedStorage.Library.Client.QuestCmds)
 
-local function updateStates(needsFarm, needsJar, needsComet)
+local function updateStates(needsFarm, needsJar, needsComet, needsHatch)
+    -- Фарм и передвижение
     _G.AutoSpeedPetsForRank = needsFarm
     _G.AutoMagnetForRank = needsFarm
     _G.AutoTeleportbestlocation = needsFarm
     _G.AutoTapForRank = needsFarm
     
+    -- Использование предметов
     _G.CoinJarUse = needsJar
     _G.CometUse = needsComet
+    
+    -- Открытие яиц
+    _G.AutoHatchBestEggForRank = needsHatch
 end
 
 task.spawn(function()
-    print("Авто-ранг менеджер (v2) запущен!")
+    print("Авто-ранг менеджер (v3) запущен!")
     
     while true do
         local needsFarm = false
         local needsJar = false
         local needsComet = false
+        local needsHatch = false
 
         if _G.Autorank then
             local data = Save.Get()
@@ -28,24 +34,33 @@ task.spawn(function()
                 for _, goal in pairs(data.Goals) do
                     local title = string.lower(QuestCmds.MakeTitle(goal))
                     
+                    -- 1. Квесты в лучшей зоне
                     if string.find(title, "best area") then
                         needsFarm = true
                     end
                     
+                    -- 2. Квесты на Coin Jar
                     if string.find(title, "coin jar") then
                         needsJar = true
                         needsFarm = true
                     end
 
+                    -- 3. Квесты на Кометы
                     if string.find(title, "comet") then
                         needsComet = true
                         needsFarm = true
+                    end
+
+                    -- 4. Квесты на открытие ЛУЧШИХ яиц (Hatch best egg)
+                    if string.find(title, "hatch") and string.find(title, "best egg") then
+                        needsHatch = true
                     end
                 end
             end
         end
 
-        updateStates(needsFarm, needsJar, needsComet)
+        -- Обновляем все переменные
+        updateStates(needsFarm, needsJar, needsComet, needsHatch)
         
         task.wait(2)
     end
