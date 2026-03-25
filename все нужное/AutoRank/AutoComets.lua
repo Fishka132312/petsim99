3de6a01728a6803d064e7ae28b19730a40db3296
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-25 13:55:50 +0100

    Update AutoComets.lua

--- все нужное/AutoRank/AutoComets.lua
+++ все нужное/AutoRank/AutoComets.lua
@@ -1,14 +1,43 @@
 _G.CometUse = false
+local SPAWN_DELAY = 3
 
-local network = game:GetService("ReplicatedStorage"):WaitForChild("Network")
-local comet = network:WaitForChild("Comet_Spawn")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Library = ReplicatedStorage:WaitForChild("Library")
+local SaveModule = require(Library.Client.Save)
+local Network = require(Library.Client.Network)
 
-local args = { "171d402a1ea141fc97bfe3b3eb6231cb" }
-
-while true do
-    if _G.CometUse then
-        comet:InvokeServer(unpack(args))
+local function getCometUID()
+    local playerData = SaveModule.Get()
+    if playerData and playerData.Inventory and playerData.Inventory.Misc then
+        for uid, item in pairs(playerData.Inventory.Misc) do
+            if item.id == "Comet" then 
+                return uid
+            end
+        end
     end
-    
-    task.wait(1)
+    return nil
 end
+
+task.spawn(function()
+    print("Авто-спавн комет запущен!")
+    
+    while true do
+        if _G.CometUse then
+            local cometUID = getCometUID()
+            
+            if cometUID then
+                local success = Network.Invoke("Comet_Spawn", cometUID)
+                
+                if success then
+                    print("Комета вызвана! UID: " .. cometUID)
+                else
+                    print("Сервер отклонил вызов (возможно, лимит на локации)")
+                end
+            else
+                warn("Кометы закончились!")
+            end
+        end
+        
+        task.wait(SPAWN_DELAY)
+    end
+end)
