b863bd3ad83e92906613ba2e84af8891bbcc8096
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-25 18:08:37 +0100

    Update AutoCollectRanks.lua

--- все нужное/things/AutoCollectRanks.lua
+++ все нужное/things/AutoCollectRanks.lua
@@ -1,26 +1,36 @@
 _G.AutoCollectRANK = false
 
-task.spawn(function()
-    local network = game:GetService("ReplicatedStorage"):WaitForChild("Network")
-    local remote = network:WaitForChild("Ranks_ClaimReward")
-    
-    print("Система сбора рангов загружена. Ожидание (AutoCollectRANK = true)...")
+local Save = require(game.ReplicatedStorage.Library.Client.Save)
+local RanksUtil = require(game.ReplicatedStorage.Library.Util.RanksUtil)
+local Directory = require(game.ReplicatedStorage.Library.Directory)
+local Network = require(game.ReplicatedStorage.Library.Client.Network)
 
+local function claimAllAvailable()
+    local data = Save.Get()
+    if not data then return end
+
+    local rankID = RanksUtil.RankIDFromNumber(data.Rank)
+    local rankData = Directory.Ranks[rankID]
+    if not rankData or not rankData.Rewards then return end
+
+    local currentStars = data.RankStars
+    local accumulatedRequired = 0
+
+    for index, reward in ipairs(rankData.Rewards) do
+        accumulatedRequired = accumulatedRequired + reward.StarsRequired
+        
+        if currentStars >= accumulatedRequired and data.RedeemedRankRewards[tostring(index)] == nil then
+            Network.Fire("Ranks_ClaimReward", index)
+            task.wait(0.5)
+        end
+    end
+end
+
+task.spawn(function()
     while true do
         if _G.AutoCollectRANK then
-            print("--- ЗАПУЩЕН СБОР РАНГОВ (1-20) ---")
-
-            for i = 1, 20 do
-                if not _G.AutoCollectRANK then break end
-                
-                remote:FireServer(i)
-                
-                task.wait(0.2) 
-            end
-            
-            task.wait(5)
-        else
-            task.wait(1)
+            pcall(claimAllAvailable)
         end
+        task.wait(5)
     end
 end)
