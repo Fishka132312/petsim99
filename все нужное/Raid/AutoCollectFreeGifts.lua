c91ecffca57ec5959bab5a6984bcb70330ad4bf9
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-27 22:03:59 +0100

    Add files via upload

---
+++ все нужное/Raid/AutoCollectFreeGifts.lua
@@ -0,0 +1,27 @@
+_G.AutoCollectGifts = true
+
+task.spawn(function()
+    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+    local network = ReplicatedStorage:WaitForChild("Network")
+    local giftRemote = network:WaitForChild("Redeem Free Gift")
+    
+    print("Система сбора подарков готова. Ожидание (AutoCollectGifts = true)...")
+
+    while true do
+        if _G.AutoCollectGifts then
+            for i = 1, 12 do
+                if not _G.AutoCollectGifts then break end
+                
+                pcall(function()
+                    giftRemote:InvokeServer(i)
+                end)
+                
+                task.wait(0.5)
+            end
+            
+            task.wait(60)
+        else
+            task.wait(1)
+        end
+    end
+end)
