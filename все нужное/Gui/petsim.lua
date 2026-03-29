49019d4655481d2b16a4d253061864bd8304e1fc
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-29 22:24:46 +0200

    Update petsim.lua

--- все нужное/Gui/petsim.lua
+++ все нужное/Gui/petsim.lua
@@ -28,7 +28,6 @@ local scripts = {
     'AutoHatchLegendary.lua',
 	'Raid/AutoRaidEbatLegit.lua',
 	'Raid/Raidfarm.lua',
-	'Antilag/BreakablesOptimizer.lua',
 	
 }
 
@@ -571,23 +570,17 @@ Tab:AddButton({
     end    
 })
 
-Tab:AddToggle({
-    Name = "Breakables Optimizer",
-    Default = false,
-	Save = true,
-    Flag = "BreakablesOptimizer",
-    Callback = function(Value)
-	_G.OptimizeBreakables = Value
+Tab:AddButton({
+	Name = "Breakables Optimizer",
+	Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Antilag/BreakablesOptimizer.lua'))()
     end    
 })
 
-Tab:AddToggle({
-    Name = "Pets Optimizer",
-    Default = false,
-	Save = true,
-    Flag = "PetsOptimizer",
-    Callback = function(Value)
-	 _G.OptimizePets = Value
+Tab:AddButton({
+	Name = "Pets Optimizer",
+	Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Antilag/PetsOptimizer.lua'))()
     end    
 })
 
