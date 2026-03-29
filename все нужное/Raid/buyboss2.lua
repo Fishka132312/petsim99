b06a8ab22eaf94ec5b64d4e6892129c913f46975
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-29 17:35:34 +0200

    Update buyboss2.lua

--- все нужное/Raid/buyboss2.lua
+++ все нужное/Raid/buyboss2.lua
@@ -1,24 +1,12 @@
--- Инициализируем переменную (по умолчанию включено)
-_G.BuyBoss2 = false
-
 while true do
-    -- Проверяем глобальную переменную
-    if _G.Buy then
-        local args = {
-            2
-        }
+local args = {
 
-        -- Выполняем покупку
-        game:GetService("ReplicatedStorage")
-            :WaitForChild("Network")
-            :WaitForChild("Raids_BossChallenge")
-            :InvokeServer(unpack(args))
-            
-        print("Скрипт работает: Покупка сделана")
-    else
-        -- Если false, просто выводим статус (можно убрать print, если мешает)
-        print("Скрипт на паузе: _G.Buy установлен в false")
-    end
+2
 
-    task.wait(2) -- Пауза между итерациями
+
+}
+
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Raids_BossChallenge"):InvokeServer(unpack(args))
+wait(2)
 end
