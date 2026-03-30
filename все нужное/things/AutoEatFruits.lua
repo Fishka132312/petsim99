006ce4f6ac923ca8f9a1ca69cffa409a8bacb2ce
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-30 07:33:39 +0200

    Update and rename AutEatFruits.lua to AutoEatFruits.lua

--- все нужное/things/AutoEatFruits.lua
+++ все нужное/things/AutoEatFruits.lua
@@ -1,4 +1,5 @@
 _G.EatFruits = false
+
 local Network = require(game:GetService("ReplicatedStorage").Library.Client.Network)
 local Save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
 
@@ -16,21 +17,22 @@ end
 
 task.spawn(function()
     
-    while _G.EatFruits do
-        local fruits = getMyFruits()
-        
-        if #fruits > 0 then
-            for _, fruitHash in ipairs(fruits) do
-                if not _G.EatFruits then break end
-                
-                Network.Fire("Fruits: Consume", fruitHash, 1)
-                
-                task.wait(0.1) 
+    while true do
+        if _G.EatFruits then
+            local fruits = getMyFruits()
+            
+            if #fruits > 0 then
+                for _, fruitHash in ipairs(fruits) do
+                    if not _G.EatFruits then break end 
+                    
+                    Network.Fire("Fruits: Consume", fruitHash, 1)
+                    task.wait(0.1)
+                end
+            else
+                task.wait(2) 
             end
-        else
-            task.wait(5)
         end
         
-        task.wait(0.5)
+        task.wait(0.5) 
     end
 end)
