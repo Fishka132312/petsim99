a0af27d642a03f8f062ed67b6f9adb8f84f4024c
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-04-02 15:42:21 +0200

    Update petsim.lua

--- все нужное/Gui/petsim.lua
+++ все нужное/Gui/petsim.lua
@@ -726,14 +726,6 @@ Tab:AddTextbox({
 		end
 	end	  
 })
-
-Tab:AddButton({
-	Name = "Total Gvno-Graphix💀",
-	Callback = function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Antilag/PetsOptimizer.lua'))()
-    end    
-})
-
 --------------------------------MISC-----------------------------
 
 local Tab = Window:MakeTab({
