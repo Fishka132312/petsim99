d936c16a4a4b42214bca021a8223b56dc2156af9
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-04-21 15:22:11 +0200

    Update turnfarmeverywhere.lua

--- все нужное/Easter2026/turnfarmeverywhere.lua
+++ все нужное/Easter2026/turnfarmeverywhere.lua
@@ -1,17 +1,37 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Library = ReplicatedStorage:WaitForChild("Library", 10)
 
+if _G.FarmEveryWhere == nil then
+    _G.FarmEveryWhere = false
+end
+
 if Library then
     local success, AutoFarmCmds = pcall(function()
         return require(Library.Client.AutoFarmCmds)
     end)
 
     if success and AutoFarmCmds then
-        pcall(function()
-            if not AutoFarmCmds.IsEnabled() then
-                AutoFarmCmds.Enable()
-            else
-                print("da blya")
+        task.spawn(function()
+            while true do
+                local isEnabled = false
+                
+                pcall(function()
+                    isEnabled = AutoFarmCmds.IsEnabled()
+                end)
+
+                if _G.FarmEveryWhere then
+                    if not isEnabled then
+                        pcall(function() AutoFarmCmds.Enable() end)
+                        print("AutoFarmEveryWhere: Enabled")
+                    end
+                else
+                    if isEnabled then
+                        pcall(function() AutoFarmCmds.Disable() end)
+                        print("AutoFarmEveryWhere: Disabled")
+                    end
+                end
+                
+                task.wait(1)
             end
         end)
     end
